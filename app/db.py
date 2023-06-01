ee3700eddab5aa9a25dca9bbe6fa717a3556e683
adhy-p <adhyp02@gmail.com>
2023-06-01 22:32:25 +0800

    Add docstring

--- app/db.py
+++ app/db.py
@@ -3,6 +3,11 @@ import sqlite3
 
 
 class DatabaseManager:
+    """
+    DatabaseManager is our own convenience class to make
+    interacting with sqlite3 database easier
+    """
+
     def __init__(self, db_path: str):
         self.connection = sqlite3.connect(db_path)
 
@@ -16,6 +21,9 @@ class DatabaseManager:
             return cursor
 
     def create_table(self, table_name: str, columns: Dict[str, str]):
+        """
+        Create a new table in the database if it does not exist yet.
+        """
         typed_cols = [f"{col} {type_}" for col, type_ in columns.items()]
         statement = f"""
             CREATE TABLE IF NOT EXISTS {table_name}
@@ -24,15 +32,14 @@ class DatabaseManager:
         self._execute(statement)
 
     def add(self, table_name: str, entries: Dict[str, str]):
+        """
+        Add a new entry to the specified table.
+
+        Parameters:
+            table_name (str) : name of the table
+            entries (dict) : map of {'column_name': 'value'}
+        """
         placeholders = f"({', '.join('?' * len(entries))})"
-        """
-        Doing something like
-        placeholders = tuple("?" * len(entries))
-        does not work.
-        This is due to the extraneous quoute around the placeholder:
-        expected   : (?, ?, ?)
-        using tuple: ('?', '?', '?')
-        """
         col_names = ", ".join(entries.keys())
         statement = f"""
             INSERT INTO {table_name}
@@ -43,6 +50,17 @@ class DatabaseManager:
         self._execute(statement, col_values)
 
     def delete(self, table_name: str, criteria: Dict[str, str]):
+        """
+        Delete entries from the specified table which matches
+        the specified criteria.
+
+        For example, if the criteria is {'id': 1, 'name': 'alice'},
+        then all entries with id = 1 and name = alice will be deleted.
+
+        Parameters:
+            table_name (str) : name of the table
+            criteria (dict) : conditions to be met
+        """
         placeholders = [f'{col} = ?' for col in criteria.keys()]
         delete_criteria = " AND ".join(placeholders)
         statement = f"""
@@ -55,6 +73,16 @@ class DatabaseManager:
     def select(self, table_name: str,
                criteria: Dict[str, str] = None,
                order_by: str = None):
+        """
+        Select entries from the specified table which matches
+        the specified criteria and sort using the specified key.
+
+        Parameters:
+            table_name (str) : name of the table
+        Optional parameters:
+            criteria (dict) : conditions to be met
+            order_by (str) : key used to sort the entries
+        """
         statement = f"""
             SELECT * FROM {table_name}
             """
